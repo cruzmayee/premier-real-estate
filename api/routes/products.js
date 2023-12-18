43aa3c30707b3c4ae839fb360555328b7fb4bc2d
jonestly-source <marjones.lapinig@gmail.com>
2023-12-18 19:00:34 +0800

    checking error

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,39 +1,39 @@
 const express = require("express");
 const router = express.Router();
-// const multer = require('multer');
+const multer = require('multer');
 const checkAuth = require("../middleware/check-auth");
 const ProductsController = require("../controllers/products");
 
-// const storage = multer.diskStorage({
-//     destination: function(req, file, cb){
-//         cb(null, 'uploads/');
-//     },
-//     filename: function(req, file,cb){
-//         // const date = new Date().toISOString()
-//         cb(null, file.originalname);
-//     }
-// });
-//
-// const fileFilter = (req, file, cb) =>{
-//     // reject a file
-//     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
-//         cb (null, true);
-//     } else{
-//         cb (null, false);
-//     }
-// };
-//
-// const upload = multer({
-//     storage: storage,
-// limits:{
-//     fileSize: 1024 * 1024 * 5
-// },
-// fileFilter: fileFilter
-// });
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, 'uploads/');
+    },
+    filename: function(req, file,cb){
+        // const date = new Date().toISOString()
+        cb(null, file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) =>{
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb (null, true);
+    } else{
+        cb (null, false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+limits:{
+    fileSize: 1024 * 1024 * 5
+},
+fileFilter: fileFilter
+});
 
 router.get("/", ProductsController.products_get_all);
 
-router.post("/", checkAuth, ProductsController.products_create_product);
+router.post("/", checkAuth, upload.single('productImage'), ProductsController.products_create_product);
 
 router.get("/:productID", checkAuth, ProductsController.products_get_product);
 
